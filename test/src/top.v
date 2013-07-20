5bf34d5f809ef54a4a585ada6d87bafad16ca437
Vsevolod Lobko <seva@sevik.org>
2013-07-20 15:39:01 +0300

    uart with DDS baudrate generator
    works at least at "230400" with cubieboard
    
    real baudrate of cubieboard at 230400 is 214300

--- test/src/top.v
+++ test/src/top.v
@@ -1,52 +1,30 @@
-////////////////////////////////////////////////////////////////////////////////
-// Copyright (c) 1995-2011 Xilinx, Inc.  All rights reserved.
-////////////////////////////////////////////////////////////////////////////////
-//   ____  ____ 
-//  /   /\/   / 
-// /___/  \  /    Vendor: Xilinx 
-// \   \   \/     Version : 13.3
-//  \   \         Application : sch2hdl
-//  /   /         Filename : top.vf
-// /___/   /\     Timestamp : 09/19/2012 01:12:31
-// \   \  /  \ 
-//  \___\/\___\ 
-//
-//Command: sch2hdl -intstyle ise -family virtex5 -verilog /home/seva/src/xc4v_test/top.vf -w /home/seva/src/xc4v_test/top.sch
-//Design Name: top
-//Device: virtex5
-//Purpose:
-//    This verilog netlist is translated from an ECS schematic.It can be 
-//    synthesized and simulated, but it should not be modified. 
-//
-`timescale 100 ps / 10 ps
+module dds_uart_clock(
+        clk,
+        baudrate,
+        enable_16
+    );
 
-module CB16CE_HXILINX_top(CEO, Q, TC, C, CE, CLR);
-   
-   parameter TERMINAL_COUNT = 16'b1111_1111_1111_1111;
-   
-   output             CEO;
-   output [15:0]      Q;
-   output             TC;
+    input clk;
+    input [15:0] baudrate;
+    output reg enable_16;
 
-   input 	      C;	
-   input 	      CE;	
-   input 	      CLR;	
-   
-   reg    [15:0]      Q;
-   
-   always @(posedge C or posedge CLR)
-     begin
-	if (CLR)
-	  Q <= 16'b0000_0000_0000_0000;
-	else if (CE)
-	  Q <= Q + 1;
-     end
-   
-   assign CEO = TC & CE;
-   assign TC = CLR ? 1'b0 : (Q == TERMINAL_COUNT);
-   
+    reg [17:0] accum = 0;
+
+    always @(posedge clk)
+    begin
+        if (accum > 62500)
+            begin
+                accum <= accum + baudrate - 62500;
+                enable_16 <= 1;
+            end
+        else
+            begin
+                accum <= accum + baudrate;
+                enable_16 <= 0;
+            end
+    end
+
 endmodule
-`timescale 1ns / 1ps
 
 module top(osc_clk, 
            rxd, 
@@ -61,7 +39,7 @@ module top(osc_clk,
            txd);
 
     input osc_clk;
-    input rxd;
+
    output led0;
    output led1;
    output led2;
@@ -70,49 +48,60 @@ module top(osc_clk,
    output led5;
    output led6;
    output led7;
+
+    input rxd;
    output txd;
    
-   wire [31:0] q;
-   wire XLXN_4;
-   wire XLXN_12;
-   wire XLXN_23;
-   wire XLXN_26;
-   wire XLXN_29;
+   reg [31:0] cnt = 0;
+
+   wire enable_16;
+   wire rst;
+   wire [7:0] rx_data;
+   wire [7:0] tx_data;
+   wire rx_done;
+   wire tx_wr;
+   wire tx_done;
    
-   (* HU_SET = "XLXI_1_0" *) 
-   CB16CE_HXILINX_top  XLXI_1 (.C(osc_clk), 
-                              .CE(XLXN_4), 
-                              .CLR(XLXN_12), 
-                              .CEO(XLXN_26), 
-                              .Q(q[15:0]), 
-                              .TC());
-   VCC  XLXI_3 (.P(XLXN_4));
-   GND  XLXI_4 (.G(XLXN_12));
-   BUF  XLXI_5 (.I(q[15]), 
-               .O(led0));
-   BUF  XLXI_6 (.I(q[14]), 
-               .O(led1));
-   BUF  XLXI_7 (.I(q[13]), 
-               .O(led2));
-   BUF  XLXI_9 (.I(q[25]), 
-               .O(led3));
-   BUF  XLXI_10 (.I(XLXN_23), 
-                .O(led4));
-   BUF  XLXI_11 (.I(XLXN_29), 
-                .O(led5));
-   BUF  XLXI_12 (.I(XLXN_23), 
-                .O(led6));
-   BUF  XLXI_13 (.I(XLXN_29), 
-                .O(led7));
-   GND  XLXI_14 (.G(XLXN_29));
-   VCC  XLXI_15 (.P(XLXN_23));
-   (* HU_SET = "XLXI_16_1" *) 
-   CB16CE_HXILINX_top  XLXI_16 (.C(osc_clk), 
-                               .CE(XLXN_26), 
-                               .CLR(XLXN_12), 
-                               .CEO(), 
-                               .Q(q[31:16]), 
-                               .TC());
-   BUF  XLXI_17 (.I(rxd), 
-                .O(txd));
+   reg blym = 0;
+
+   assign led0 = rx_data[0];
+   assign led1 = rx_data[1];
+   assign led2 = rx_data[2];
+   assign led3 = rx_data[3];
+   assign led4 = rx_data[4];
+   assign led5 = rx_data[5];
+   assign led6 = rx_data[6];
+   assign led7 = rx_data[7];
+
+   assign tx_data = rx_data;
+   assign tx_wr = rx_done;
+   assign rst = 0;
+
+    always @(posedge osc_clk)
+    begin
+        cnt <= cnt + 1;
+        if (enable_16)
+            blym <= ~blym;
+    end
+
+    dds_uart_clock uclock1(
+        .clk(osc_clk),
+        .baudrate(2142),
+        .enable_16(enable_16)
+    );
+
+    uart_transceiver uart1(
+        .sys_clk(osc_clk),
+        .sys_rst(rst),
+        .uart_rx(rxd),
+        .uart_tx(txd),
+        .enable_16(enable_16),
+        .rx_data(rx_data),
+        .rx_done(rx_done),
+        .tx_data(tx_data),
+        .tx_wr(tx_wr),
+        .tx_done(tx_done)
+    );
+
+
 endmodule
