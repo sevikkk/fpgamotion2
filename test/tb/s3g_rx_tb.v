96a87c10c4ff3f203f80f99f5e3d21a85506125e
Vsevolod Lobko <seva@sevik.org>
2013-07-27 14:38:32 +0300

    s3g sender with testbench

--- test/tb/s3g_rx_tb.v
+++ test/tb/s3g_rx_tb.v
@@ -1,4 +1,5 @@
 `include "../src/s3g_rx.v"
+`include "../src/s3g_tx.v"
 
 module s3g_rx_tb;
 
@@ -7,12 +8,27 @@ module s3g_rx_tb;
     reg clk;
     reg rst;
     reg [31:0] cycle;
+    reg packet_wr;
+    reg tx_done;
+    wire tx_wr;
 
     s3g_rx dut(
         .clk(clk),
         .rst(rst),
         .rx_data(rx_data),
         .rx_done(rx_done)
+    );
+
+    s3g_tx dut2(
+        .clk(clk),
+        .rst(rst),
+        .tx_done(tx_done),
+        .tx_wr(tx_wr),
+        .payload_len(8'h3),
+        .packet_wr(packet_wr),
+        .buf0(8'h1),
+        .buf1(8'h2),
+        .buf2(8'h3)
     );
 
     initial begin
@@ -28,6 +44,12 @@ module s3g_rx_tb;
 
                 if (rx_done == 1)
                     rx_done = 0;
+
+                if (tx_done == 1)
+                    tx_done = 0;
+
+                if (packet_wr == 1)
+                    packet_wr = 0;
 
                 case (cycle)
                     10: begin
@@ -58,6 +80,15 @@ module s3g_rx_tb;
                         rx_data = 8'hCC;
                         rx_done = 1;
                     end
+                    80: begin
+                            packet_wr = 1;
+                        end
+                    90: tx_done = 1;
+                    100: tx_done = 1;
+                    110: tx_done = 1;
+                    120: tx_done = 1;
+                    130: tx_done = 1;
+                    140: tx_done = 1;
                 endcase
 
                 #2;
