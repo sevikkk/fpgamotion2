754d5c37830f6a18478d2424e54aa4788997c050
Vsevolod Lobko <seva@sevik.org>
2013-07-27 13:25:39 +0000

    less chatty version

--- soft/talk.py
+++ soft/talk.py
@@ -7,18 +7,14 @@ from traceback import print_exc
 import logging
 
 def test():
-    logging.basicConfig(level=logging.DEBUG, format="%(levelname)s: %(message)s")
+    logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
     bot = s3g.from_filename("/dev/ttyS1", Condition(), baudrate=921600)
     bot.open()
     try:
         v = bot.get_version()
-        print v
+        print "Bot version: %X" % v
     except:
         print_exc()
-
-    time.sleep(1000)
-
-
 
 if __name__ == "__main__":
     test()
