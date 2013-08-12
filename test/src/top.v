55f4f105e6589a36a72cde51043d55ad169de863
Vsevolod Lobko <seva@sevik.org>
2013-08-12 13:57:10 +0300

    standart delay for interrupt resend

--- test/src/top.v
+++ test/src/top.v
@@ -183,7 +183,7 @@ s3g_tx s3g_tx1(
            .buf15(tx_buf15)
        );
 
-s3g_executor #(100) s3g_exec1(
+s3g_executor s3g_exec1(
              .clk(osc_clk),
              .rst(rst),
              .rx_packet_done(rx_packet_done),
