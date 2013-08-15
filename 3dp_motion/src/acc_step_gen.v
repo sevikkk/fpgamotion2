e7af8402415697c151b6d8df9e664525e458bb27
Vsevolod Lobko <seva@sevik.org>
2013-08-15 18:14:49 +0300

    ability to setup next step in advance

--- 3dp_motion/src/acc_step_gen.v
+++ 3dp_motion/src/acc_step_gen.v
@@ -4,6 +4,10 @@ module acc_step_gen(
            input [31:0] dt_val, // Step interval
            input [31:0] steps_val, // Number of steps in current sequence
            input load,
+           input set_steps_limit,
+           input set_dt_limit,
+           input reset_steps,
+           input reset_dt,
            output reg [31:0] steps,
            output reg [31:0] dt,
            output reg abort,           // combinatorial
@@ -16,14 +20,21 @@ localparam S_INIT = 0, S_WORKING = 1, S_WAIT = 2, S_ABORT = 3;
 reg [31:0] next_dt;
 reg [31:0] next_steps;
 
+reg [31:0] steps_limit;
+reg [31:0] next_steps_limit;
+reg [31:0] dt_limit;
+reg [31:0] next_dt_limit;
+
 reg [2:0] state;
 reg [2:0] next_state;
 
-always @(state, load, dt_val, steps_val, steps, dt, reset)
+always @(state, load, dt_val, steps_val, steps, dt, reset, steps_limit, dt_limit, set_steps_limit, set_dt_limit, reset_steps, reset_dt)
     begin
         next_state <= state;
-        next_dt <= dt;
+        next_dt <= dt + 1;
         next_steps <= steps;
+        next_steps_limit <= steps_limit;
+        next_dt_limit <= dt_limit;
 
         abort <= 0;
         step_stb <= 0;
@@ -34,37 +45,39 @@ always @(state, load, dt_val, steps_val, steps, dt, reset)
                 next_state <= S_INIT;
                 next_dt <= 0;
                 next_steps <= 0;
+                next_steps_limit <= 0;
+                next_dt_limit <= 0;
             end
         else
+            if (load)
+                begin
+                    if (reset_dt)
+                        next_dt <= 0;
+                    if (reset_steps)
+                        next_steps <= 0;
+                    if (set_steps_limit)
+                        next_steps_limit <= steps_val;
+                    if (set_dt_limit)
+                        next_dt_limit <= dt_val;
+                end
             case (state)
                 S_INIT:
                     begin
                         if (load)
-                            begin
-                                next_dt <= 0;
-                                next_steps <= 0;
-                                next_state <= S_WORKING;
-                            end
+                            next_state <= S_WORKING;
                     end
                 S_WORKING:
                     begin
-                        if (load) // Restart current step
+                        if (!load)
                             begin
-                                next_dt <= 0;
-                                next_steps <= 0;
-                            end
-                        else
-                            begin
-                                next_dt <= dt + 1;
-                                if (dt + 1 >= dt_val)
+                                if (dt + 1 >= dt_limit)
                                     begin
                                         next_dt <= 0;
                                         next_steps <= steps + 1;
                                         step_stb <= 1;
-                                        if (steps + 1 >= steps_val)
+                                        if (steps + 1 >= steps_limit)
                                             begin
                                                 done <= 1;
-                                                next_steps <= 0;
                                                 next_state <= S_WAIT;
                                             end
                                     end
@@ -72,17 +85,14 @@ always @(state, load, dt_val, steps_val, steps, dt, reset)
                     end
                 S_WAIT:
                     begin
-                        next_dt <= dt + 1;
                         if (load) // new data loaded in time
-                            begin
-                                next_steps <= 0;
-                                next_state <= S_WORKING;
-                            end
+                            next_state <= S_WORKING;
                         else // timeout waiting for next data, aborting
                             begin
-                                if (dt + 1 >= dt_val)
+                                if (dt + 1 >= dt_limit)
                                     begin
                                         next_dt <= 0;
+                                        next_steps <= steps + 1;
                                         abort <= 1;
                                         step_stb <= 1;
                                         next_state <= S_ABORT;
@@ -91,18 +101,14 @@ always @(state, load, dt_val, steps_val, steps, dt, reset)
                     end
                 S_ABORT: // data not available, run abort sequence - generate step_stb's with abort set
                     begin
-                        next_dt <= dt + 1;
                         if (load) // RESTART
-                            begin
-                                next_dt <= 0;
-                                next_steps <= 0;
-                                next_state <= S_WORKING;
-                            end
+                            next_state <= S_WORKING;
                         else
                             begin
                                 abort <= 1;
                                 if (dt + 1 >= dt_val)
                                     begin
+                                        next_steps <= steps + 1;
                                         next_dt <= 0;
                                         step_stb <= 1;
                                     end
@@ -114,7 +120,9 @@ always @(state, load, dt_val, steps_val, steps, dt, reset)
 always @(posedge clk)
     begin
         dt <= next_dt;
+        dt_limit <= next_dt_limit;
         steps <= next_steps;
+        steps_limit <= next_steps_limit;
         state <= next_state;
     end
 
